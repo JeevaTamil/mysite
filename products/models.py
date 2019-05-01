b369035e703e81c68b946024306910d51554681e
Azhagusundaram.t <jeeva_tamil@yahoo.in>
2019-05-01 22:17:41 +0530

    TextField changed to appropriate fields for each elements

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    title       = models.TextField() 
-    description = models.TextField()
-    price       = models.TextField()
+    title       = models.CharField(max_length=120) 
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(max_digits=100000, decimal_places=2)
     summary     = models.TextField(default = "this is summary cool!!!!")
